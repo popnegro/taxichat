5e173ed9dfd157783b5126803db0ba00e0e4109e
popnegro <grasso.luis@gmail.com>
2026-04-13 14:42:50 -0300

    Versión PMV

--- api/socket.js
+++ api/socket.js
@@ -1,33 +1,34 @@
 import { Server } from 'socket.io';
 
 export default function handler(req, res) {
-  if (res.socket.server.io) {
-    res.end();
-    return;
+  if (!res.socket.server.io) {
+    console.log('--- Inicializando Socket.io ---');
+    const io = new Server(res.socket.server, {
+      path: '/api/socket',
+      addTrailingSlash: false,
+      cors: {
+        origin: '*', // Permitir conexiones de cualquier origen
+      },
+    });
+
+    io.on('connection', (socket) => {
+      console.log('Conexión establecida:', socket.id);
+
+      // ESCUCHAR: El evento que viene del client.html
+      socket.on('nuevo-viaje', (data) => {
+        console.log('Nuevo viaje recibido de un cliente:', data);
+        
+        // EMITIR: Reenviar a todos (incluyendo al operator.html)
+        // Usamos io.emit para que llegue a todas las ventanas abiertas
+        io.emit('viaje-recibido', data);
+      });
+
+      socket.on('disconnect', () => {
+        console.log('Usuario desconectado');
+      });
+    });
+
+    res.socket.server.io = io;
   }
-
-  const io = new Server(res.socket.server, {
-    path: '/api/socket',
-    addTrailingSlash: false,
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"]
-    }
-  });
-
-  res.socket.server.io = io;
-
-  io.on('connection', (socket) => {
-    console.log('Cliente conectado:', socket.id);
-    
-    socket.on('nuevo-viaje', (data) => {
-      socket.broadcast.emit('viaje-recibido', data);
-    });
-
-    socket.on('disconnect', () => {
-      console.log('Cliente desconectado');
-    });
-  });
-
   res.end();
 }
